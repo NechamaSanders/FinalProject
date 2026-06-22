92174037ea94b136d0525e62b455f8f04baeb937
Nechama Sanders <nechamasanders2006@gmail.com>
2026-06-22 15:34:08 +0300

    Moved dependency injection to separate project

---
+++ FinalProject.Common/DependencyContainer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using FinalProject.BL.Services;
+using FinalProject.BL; 
+using FinalProject.DAL;    
+using FinalProject.DAL.Repositories;
+namespace FinalProject.Common
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection RegisterSystemServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+      
+            services.AddScoped<IRecipeRepository, RecipeRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IIngredientRepository, IngredientRepository>();
+
+            services.AddScoped<IRecipeService, RecipeService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IIngredientService, IngredientService>();
+
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            return services;
+        }
+    }
+}
